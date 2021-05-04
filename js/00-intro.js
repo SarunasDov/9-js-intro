57e492a49f880916ad87f3936eddb4b530f2a012
Sarunas <sar.dovydaitis@hotmail.com>
2021-05-04 18:56:01 +0300

    innitial commit

---
+++ js/00-intro.js
@@ -0,0 +1,24 @@
+// single-line comment
+
+/*
+multi
+line
+comment
+*/
+
+/*
+
+KONCEPTAI:
+
+1) kintamieji - isimena informacija  
+2) salygos sakiniai - tarpusio palyginimas
+3) ciklai - darbas su pasikartojancio formato informacija,
+pasikartojanciu procesu valdymas
+4) funkcijos - uzdara logika, kuria galima perpanaudoti pagal poreiki
+5) ivykiai - uzsikrove puslapis, paspaude mygtuka, paspaude peles mygtuka
+pradejo siusti faila, pabaige siusti faila, issiunte uzklausa ir t.t.
+6) oop - +- kaip 4-tame funkcijos, objektinis programavimas - tai
+tik "skonio" reikalas
+
+
+*/
