cfdc4f67d9efa08a162ee7309c96bc3d35713b72
Sarunas <sar.dovydaitis@hotmail.com>
2021-05-06 18:49:23 +0300

    ternary

---
+++ js/09-ternary.js
@@ -0,0 +1,33 @@
+/*
+
+Ternary OPERATOR
+
+              true      false
+klausimas ? teigiama : neigiama
+
+
+*/
+
+const arLyja = true;
+
+const sketis = arLyja ? 'pasiimk' : 'nereikia';            //pasiimk / nereikia
+console.log(sketis);
+
+
+const s1 = 4 < 2 ? 'daugiau' : 'ne daugiau';
+console.log(s1);
+
+
+let s2 = '';
+if (4 < 2) {
+    s2 = daugiau;
+} else {
+    s2 = 'ne daugiau';
+}
+console.log(s2);
+
+let s3 = 'ne daugiau';
+if (4 < 2) {
+    s3 = 'daugiau';
+}
+console.log(s3);
