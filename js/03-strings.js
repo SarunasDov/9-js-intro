3656adee25c2b0cb853cbcfdd9ec15085548749f
Sarunas <sar.dovydaitis@hotmail.com>
2021-05-06 18:02:59 +0300

    strings

--- js/03-strings.js
+++ js/03-strings.js
@@ -1,11 +1,9 @@
 /*
 STRINGS (tekstas)
-
 inicijavimo budai:
-viengubos (') kabutes
-dvigubos (") kabutes
-backtick (`) kabutes
-
+- viengubos (') kabutes
+- dvygubos (") kabutes
+- backtick (`) kabutes
 */
 
 const kabutes1 = 'Labas';
@@ -18,30 +16,37 @@ console.log(kabutes2);
 const kabutes21 = "Viengubos (') kabutes.";
 console.log(kabutes21);
 
-// Dvigubos (") kabutes.
-const kabutes12 = 'Dvigubos (") kabutes.';
+// Dvygubos (") kabutes.
+const kabutes12 = 'Dvygubos (") kabutes.';
 console.log(kabutes12);
 
-// Viengubos (') ir dvigubos (") kabutes.
-const kabutes1e = 'Viengubos (\') ir dvigubos (") kabutes.';
+// Viengubos (') ir dvygubos (") kabutes.
+const kabutes1e = 'Viengubos (\') ir dvygubos (") kabutes.';
 console.log(kabutes1e);
-
-const kabutes2e = "Viengubos (') ir dvigubos (\") kabutes.";
+const kabutes2e = "Viengubos (') ir dvygubos (\") kabutes.";
 console.log(kabutes2e);
 
-// Tekste backslash \ yra jega!
-
-const backslash = 'Tekste backslash \\ yra jega!';
+// Tekste backslash \' yra jega!
+const backslash = 'Tekste backslash \\\' yra jega!';
 console.log(backslash);
 
+const vardas = 'Vardenis';
+const pavarde = 'Pavardenis';
+
+// Vardenis Pavardenis
+const fullName = vardas + ' ' + pavarde;
+console.log(fullName);
+
+// Sveiki, as esu Vardenis!
+const sayHi = 'Sveiki, as esu ' + vardas + '!';
+console.log(sayHi);
 
 // \n - enter
 // \t - tab
 
-
 const imgSrc = './img/logo.png';
-const HTML = '<header>\
-            <img src="' + imgSrc + '" alt="logo" class="logo">\n\
+const HTML = '<header>\n\
+                <img src="' + imgSrc + '" alt="logo" class="logo">\n\
                 <nav>\n\
                     <a href="#">Link</a>\n\
                     <a href="#">Link</a>\n\
@@ -49,27 +54,22 @@ const HTML = '<header>\
                     <a href="#">Link</a>\n\
                 </nav>\n\
             </header>';
-
 console.log(HTML);
 
-
-const H0 = '<header>' +
-                '<img>' +
-                '<nav>' +
-                    '<a></a>' +
-                    '<a></a>' +
-                    '<a></a>' +
-                    '<a></a>' +
-                '<nav>' +
-            '</header>' +
-
 const H2 = `<header>
-<img src="${imgSrc}" alt="logo" class="logo">
-    <nav>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-    </nav>
-</header>`;
+                <img src="${imgSrc}" alt="logo" class="logo">
+                <nav>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                    <a href="#">Link</a>
+                </nav>
+            </header>`;
 console.log(H2);
+
+const demo = 'asd ${imgSrc} asd';
+console.log(demo);
+
+const hi = 'Good morning!';
+const hiSize = hi.length;
+console.log(hi, hiSize);
